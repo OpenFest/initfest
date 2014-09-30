23e15d96622d62243fd8981f68613250861168c1
Vladimir Vassilev <vlood.vassilev@gmail.com>
2014-09-30 13:32:53 +0300

    registering menus

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+
+register_nav_menus(
+    array( 'main-menu' => __( 'Main Menu', 'initfest' ),
+           'footer-openfest' => __('OpenFest', 'initfest'),
+           'footer-openfest' => __('OpenFest', 'initfest'),
+           'footer-schedule' => __('Schedule', 'initfest'),
+           'footer-others' => __('Footer Others', 'initfest'),
+           'footer-followus' => __('Follow us in:', 'initfest') ) )
+);
