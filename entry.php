cac56245b3e8ddfcd971a143cd2195920ee324f9
Vladimir Vassilev <vlood.vassilev@gmail.com>
2014-09-30 16:29:16 +0300

    remove comments and  meta from front-page

--- entry.php
+++ entry.php
@@ -9,8 +9,8 @@
         <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
         <?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
         <?php edit_post_link(); ?>
-        <?php if ( !is_search() && !is_page() ) get_template_part( 'entry', 'meta' ); ?>
+        <?php if ( !is_search() && !is_page() && !is_front_page()) get_template_part( 'entry', 'meta' ); ?>
     </header>
     <?php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
-    <?php if ( !is_search() && !is_page()) get_template_part( 'entry-footer' ); ?>
+    <?php if ( !is_search() && !is_page() && !is_front_page()) get_template_part( 'entry-footer' ); ?>
 </article>
