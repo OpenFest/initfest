7020dc79d976a904953e6893166a40295670ac0b
Monika Eftimova <plazmonii@ludost.net>
2014-10-06 05:41:54 +0300

    Remove sidebar from front page

--- front-page.php
+++ front-page.php
@@ -5,7 +5,6 @@
         <p><?php echo get_bloginfo('description'); ?></p>
     </div>
     <div class="col2">
-        <h3>Спонсори</h3>
         <?php echo do_shortcode( '[sponsors]' ); ?>
     </div>
 </section>
@@ -18,24 +17,7 @@
 
 <div class="separator"></div>
 
-<section class="content">
-    <h3>Място: Интерпред, София, България</h3>
-    <?php
-        $transport_args = array( 'post_type' => 'transportation' );
-        $transport = new WP_Query( $transport_args ); 
 
-        if ( $transport->have_posts() ) :
-            while ( $transport->have_posts() ) : $transport->the_post();
-    ?>
-        <p><?php the_title(); ?> <br /> <?php the_content(); ?></p>
-    <?php 
-            endwhile;
-        endif;
-    ?>
-</section>
+<?php echo do_shortcode( '[transport]' ); ?>
 
-
-<?php echo do_shortcode( '[ready_google_map id="1" map_language="en" type="HYBRID" align="right"]' ); ?>
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
