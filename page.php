12ff13d449e7625b466ce4d44428a6f8c0564e57
Vasil Kolev <vasil@ludost.net>
2014-10-14 07:18:15 +0300

    fix volunteers page submenu

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 get_header(); 
 
 /* Some special pages (the ones describing the event, team, etc. need a special submenu */
-if ( preg_match('/^(about|ideas-and-recommendations|feedback|team|history)/', $pagename) ) {
+if ( preg_match('/^(about|ideas-and-recommendations|feedback|team|history|volunteers)/', $pagename) ) {
     wp_nav_menu( array( 'theme_location' => 'subnav-menu', 'container_class' => 'content subnav cf' ) );
     echo '<div class="separator"></div>';
 }
