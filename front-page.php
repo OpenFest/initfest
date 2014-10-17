d2cad0b1505a88955969455ee48f11817ad12da0
Vasil Kolev <vasil@ludost.net>
2014-10-17 10:56:05 +0300

    reorder the main page a bit

--- front-page.php
+++ front-page.php
@@ -1,17 +1,13 @@
 <?php get_header(); ?>
-<div class="content front-page-big-title"><h1>OpenFest e единствената по рода си в България конференция, посветена на свободната култура, свободния софтуер и софтуера с отворен код, свободното споделяне на знания – фестивал на свободното творчество. OpenFest е ежегодна среща на всички почитатели, създатели, поддръжници и нови фенове на свободните изкуства и свободния софтуер.</h1></div>
+<section class="content">
+	<?php echo do_shortcode( '[sh-latest-posts cat="news" label="'.pll__('Новини').'"]' ); ?>
 <div class="separator"></div>
 <div class="col-right sponsors">
     <?php echo do_shortcode( '[sponsors]' ); ?>
     <?php echo do_shortcode( '[partners]' ); ?>
 </div>
 <div class="separator"></div>
-<section class="content">
-	<?php echo do_shortcode( '[sh-latest-posts cat="news" label="'.pll__('Новини').'"]' ); ?>
 </section>
-
-<div class="separator"></div>
-
 
 <?php echo do_shortcode( '[transport]' ); ?>
 
