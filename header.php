6f65da326032ca5465ae760ca1095d5b043b02c5
Vasil Kolev <vasil@ludost.net>
2019-06-17 19:42:18 +0300

    theme: add most stuff for 2019

--- header.php
+++ header.php
@@ -26,8 +26,13 @@
 <?php
 	$blog_details = get_blog_details();
 	$blog_slug = str_replace('/', '', $blog_details->path);
+
+	if ($blog_slug == '2019')  {
+		echo '<nav style="background: url(\''.get_template_directory_uri().'/img/navbg-2019.png\'); height: 84px">';
+	} else {
+		echo "<nav>";
+	}
 ?>
-		<nav>
 			<div class="content cf">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo"><img src="<?php echo get_template_directory_uri().'/img/logo-'.$blog_slug.'.png'; ?>" alt="OpenFest" /></a>
 				
@@ -36,6 +41,11 @@
 		</nav>
 		<?php 
 			if(openfest_home_page()){
-				echo '<section class="banner cf" style="background: url(\''.get_template_directory_uri().'/img/banner-back-'.$blog_slug.'.jpg\') top repeat-x;padding: 0.2em 0 0 0;height: 258px;"><img src="'.get_template_directory_uri().'/img/banner-'.of_get_lang().'-'. $blog_slug .'.png" alt="" /></section>';
+				if ($blog_slug == '2019') {
+					echo '<section class="banner cf" style="background: url(\''.get_template_directory_uri().'/img/banner-back-'.$blog_slug.'.jpg\') top repeat-x; background-size:cover; position:relative; padding: 0.2em 0 0 0;height: 400px;"><img src="'.get_template_directory_uri().'/img/banner-'.of_get_lang().'-'. $blog_slug .'.png" alt="" style="position:absolute;top:0;left:50%;margin-left:-550px;height:100%;width:430px" /></section>';
+					#echo '<section class="banner cf" style="background: url(\''.get_template_directory_uri().'/img/banner-back-'.$blog_slug.'.jpg\') top repeat-x;padding: 0.2em 0 0 0;height: 400px;"><img src="'.get_template_directory_uri().'/img/banner-'.of_get_lang().'-'. $blog_slug .'.png" alt="" style="padding-right: 35%; margin-top: -0.2em;;height:400px" /></section>'; 
+				} else {
+					echo '<section class="banner cf" style="background: url(\''.get_template_directory_uri().'/img/banner-back-'.$blog_slug.'.jpg\') top repeat-x;padding: 0.2em 0 0 0;height: 258px;"><img src="'.get_template_directory_uri().'/img/banner-'.of_get_lang().'-'. $blog_slug .'.png" alt="" /></section>';
+				}
 			}
 		?>
