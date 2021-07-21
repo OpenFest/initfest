0b11777948523694e476b747a03226a616d5e857
Vasil Kolev <vasil@ludost.net>
2021-07-21 18:25:56 +0300

    2021: rm comments-are-disabled

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
     <div class="col-left">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <?php get_template_part( 'entry' ); ?>
-        <?php comments_template(); ?>
         <?php endwhile; endif; ?>
         <?php get_template_part( 'nav', 'below' ); ?>
     </div>
