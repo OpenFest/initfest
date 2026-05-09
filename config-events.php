906bd1bf9a02337a6fa3fef1d3bc91b12f9d8441
Vasil Kolev <vasil@ludost.net>
2026-05-09 13:28:11 +0300

    2025: final fixes

--- config-events.php
+++ config-events.php
@@ -17,11 +17,11 @@ $eventsConfig = [
         'startTime' => '2025-10-18 09:45:00',
         'endTime' => '2025-10-19 18:15:00',
         'streams' => [[
-            'startTime' => '2025-10-18 09:45:00',
+            'startTime' => '2025-10-18 08:45:00',
             'endTime' => '2025-10-18 19:00:00',
             'tracks' => ['hall-a', 'hall-b'],
         ], [
-            'startTime' => '2025-10-19 09:45:00',
+            'startTime' => '2025-10-19 08:45:00',
             'endTime' => '2025-10-19 18:15:00',
             'tracks' => ['hall-a', 'hall-b'],
         ]],
